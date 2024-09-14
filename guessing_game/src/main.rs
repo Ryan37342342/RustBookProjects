6f69898ba1fee5881a3aee25e3bfd08c4782b5bc
Ryan McCarthy <ryanmccarthy667@gmail.com>
2024-09-14 21:14:23 +1200

    finished guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,19 +4,37 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+    // generater range is from <start> to and including <end>
     let secert_number = rand:: thread_rng().gen_range(1..=100);
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop{
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}")
-
-    match guess.cmp(&secert_number){
-        Ordering::Less => println!("Too small!")
-        Ordering::Greater => println!("Too big!")
+        // create number guess as string 
+        let mut guess = String::new();
+    
+        io::stdin()
+            // read string in from command line 
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        //use shadowing ?? and trim whitespace etc and store in an int 32 variable also called guess
+        // parse returns an enum that has two varients ok and err
+        let guess : u32 = match guess.trim().parse(){
+            //handle each possible returned result from parse t
+            Ok(num) => num,
+            // underscore is catch all so catch all possible returned errors
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secert_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!")
+                break;
+            }
+        }
     }
+   
 }
